09e42773d9d2a7afdd391bac39ceadcd17cb9f5d
Caedin Cook <caco5646@colorado.edu>
2024-09-29 22:49:01 -0600

    RUST

---
+++ rust/src/lib.rs
@@ -0,0 +1,99 @@
+// Steve Operating System
+// Stephen Marz
+// 21 Sep 2019
+#![no_std]
+use core::arch::asm;
+
+// ///////////////////////////////////
+// / RUST MACROS
+// ///////////////////////////////////
+#[macro_export]
+macro_rules! print
+{
+	($($args:tt)+) => ({
+
+	});
+}
+#[macro_export]
+macro_rules! println
+{
+	() => ({
+		print!("\r\n")
+	});
+	($fmt:expr) => ({
+		print!(concat!($fmt, "\r\n"))
+	});
+	($fmt:expr, $($args:tt)+) => ({
+		print!(concat!($fmt, "\r\n"), $($args)+)
+	});
+}
+
+// ///////////////////////////////////
+// / LANGUAGE STRUCTURES / FUNCTIONS
+// ///////////////////////////////////
+#[no_mangle]
+extern "C" fn eh_personality() {}
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+	print!("Aborting: ");
+	if let Some(_p) = info.location() {
+		println!(
+				 "line {}, file {}: {}",
+				 _p.line(),
+				 _p.file(),
+				 info.message().unwrap()
+		);
+	}
+	else {
+		println!("no information available.");
+	}
+	abort();
+}
+#[no_mangle]
+extern "C"
+fn abort() -> ! {
+	loop {
+		unsafe {
+			asm!("wfi", options(nomem, nostack, preserves_flags));
+		}
+	}
+}
+
+// ///////////////////////////////////
+// / CONSTANTS
+// ///////////////////////////////////
+
+// ///////////////////////////////////
+// / ENTRY POINT
+// ///////////////////////////////////
+#[no_mangle]
+extern "C"
+fn kmain() {
+	// Main should initialize all sub-systems and get
+	// ready to start scheduling. The last thing this
+	// should do is start the timer.
+	//sit in a while loop doing nothing
+	print_str("Hello, World!");
+	loop {}
+}
+
+fn print_char(c: char) {
+	unsafe {
+		//create a pointer to 0x10000000;
+		let ptr = 0x10000000 as *mut u32;
+		//set to 0x64
+		ptr.write_volatile(c as u32);
+		
+	}
+}
+
+fn print_str(s: &str) {
+	for c in s.chars() {
+		print_char(c);
+	}
+}
+
+// ///////////////////////////////////
+// / RUST MODULES
+// ///////////////////////////////////
+
