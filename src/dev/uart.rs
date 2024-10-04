72e284f2b9146cab1c7c0994cc5b47fcf1ff6ef6
dylan-bomgardner <dylan.m.bomgardner@gmail.com>
2024-10-04 00:28:15 -0700

    changed howe we import files and added a console

--- src/dev/uart.rs
+++ src/dev/uart.rs
@@ -1,20 +1,53 @@
-const UART: u32 = 0x1000_0000;
+use core::fmt::{Error, Write};
 
-pub struct Writer;
-impl core::fmt::Write for Writer {
+pub struct Uart {
+    addr: *mut u8,
+}
+
+impl Uart {
+    // Constructor
+    pub fn new(addr: *mut u8) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    pub fn print_str(&self, s: &str) {
+        for c in s.chars() {
+            self.print_char(c);
+        }
+    }
+    
+    pub fn print_char(&self, c: char) {
+        unsafe {
+            self.addr.write_volatile(c as u8);
+        }
+    }
+
+    pub fn read_char(&self) -> u8 {
+        unsafe {
+            return self.addr.read_volatile();
+        }
+    }
+
+    pub fn enable_fifo(&self) {
+        // Enable FIFO
+        unsafe {
+            self.addr.add(2).write_volatile(1 << 0);
+        }
+    }
+
+    pub fn enable_interrupts(&self) {
+        unsafe {
+            self.addr.add(1).write_volatile(1 << 0);
+        }
+    }
+}
+
+impl Write for Uart {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        print_str(s);
+        self.print_str(s);
         Ok(())
     }
 }
 
-pub fn print_str(s: &str) {
-    for c in s.chars() {
-        print_char(c);
-    }
-}
-pub fn print_char(c: char) {
-    unsafe {
-        core::ptr::write_volatile(UART as *mut u8, c as u8);
-    }
-}
